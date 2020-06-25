43f4bb3dbfd52453ac24419826d7e6c428e11024
tonybutzer <tonybutzer@gmail.com>
2020-06-25 17:44:32 +0000

    automatic git update from Makefile

--- veget/vegetLib/vegetLib/rastermanager.py
+++ veget/vegetLib/vegetLib/rastermanager.py
@@ -120,7 +120,10 @@ class RasterManager:
             # bucket_name = os.path.split(self.config_dict['out_root'])[0]     # dev-et-data
             # bucket_prefix = os.path.split(self.config_dict['out_root'])[-1]  # tile_modelrun1
             bucket_name = self.config_dict['out_root'].split('/')[0]
-            bucket_prefix = self.config_dict['out_root'].split('/')[1:]
+            bucket_prefix_list = self.config_dict['out_root'].split('/')[1:]
+            print(bucket_prefix_list)
+            bucket_prefix = '/'.join(bucket_prefix_list)
+            print("bucket prefix =", bucket_prefix)
             bucket_filepath = os.path.join(bucket_prefix, outname)   # os.path.join(dev-et-data/tile_modelrun1, outname)
 
             # uploads to aws bucket with filepath
