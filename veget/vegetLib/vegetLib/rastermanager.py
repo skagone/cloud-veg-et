ac66b90a3b102c16adf14a4dae24f6f3fae2c98f
tonybutzer <tonybutzer@gmail.com>
2020-06-25 17:34:25 +0000

    automatic git update from Makefile

--- veget/vegetLib/vegetLib/rastermanager.py
+++ veget/vegetLib/vegetLib/rastermanager.py
@@ -118,7 +118,7 @@ class RasterManager:
 
             # Buckets are not directories but you can treat them like they are
             bucket_name = os.path.split(self.config_dict['out_root'])[0]     # dev-et-data
-            bucket_prefix = os.path.split(self.config_dict['out_root'])[-1]  # tile_modelrun1
+            bucket_prefix = os.path.split(self.config_dict['out_root'])[1:-1]  # tile_modelrun1
             bucket_filepath = os.path.join(bucket_prefix, outname)   # os.path.join(dev-et-data/tile_modelrun1, outname)
 
             # uploads to aws bucket with filepath
