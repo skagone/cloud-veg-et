446cf19fc26c519fed12c3c79968096f7f6fc4fe
gabe-parrish <gbrlparrish@gmail.com>
2020-09-30 14:53:36 -0500

    a catch all line of code to set the crs to epsg 4326 if crs=null II _warp_one

--- vegetLib/vegetLib/rastermanager.py
+++ vegetLib/vegetLib/rastermanager.py
@@ -204,6 +204,7 @@ class RasterManager:
         for i, warpfile in enumerate(inputs):
             # print('warpfile', warpfile)
             with rasterio.open(warpfile) as src:
+                # TODO - make the default configurable.
                 if src.crs == None:
                     src.crs = CRS.from_epsg(4326)
                 # create the virtual raster based on the standard rasterio attributes from the sample tiff and shapefile feature.
@@ -235,6 +236,8 @@ class RasterManager:
         while(cnt>0):
             try:
                 with rasterio.open(warpfile) as src:
+                    if src.crs == None:
+                        src.crs = CRS.from_epsg(4326)
                     # create the virtual raster based on the standard rasterio attributes from the sample tiff and shapefile feature.
                     with WarpedVRT(src, resampling=rs,
                            crs=self.crs,
